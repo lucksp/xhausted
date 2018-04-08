d4d1477262b84f2b8994b4ac5ddf259e4ba0c795
Phil Lucks <philip.lucks@snapengage.com>
2018-04-08 17:34:31 -0600

    move static()

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV !== "production") {
   );
 } else {
   useFolder = "/dist/";
-  app.use(express.static("dist"));
 }
+app.use(express.static("dist"));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, ".." + useFolder + "index.html"));
